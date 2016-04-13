194444a5897f063ce660a684083653042e53ab79
Matthew <Matthew@MattMo.wifi.etsu.edu>
2016-04-13 16:29:22 -0400

    Player class init

---
+++ src/dungeonCrawl/Player.java
@@ -0,0 +1,26 @@
+/**
+ * ---------------------------------------------------------------------------
+ * File name: Player.java
+ * Project name: Project5-Zork
+ * ---------------------------------------------------------------------------
+ * Creator's name and email: Matthew Moore, zmjm40@goldmail.etsu.edu
+ * Course:  CSCI 1260
+ * Creation Date: Apr 13, 2016
+ * ---------------------------------------------------------------------------
+ */
+
+package dungeonCrawl;
+
+
+/**
+ * Enter type purpose here
+ *
+ * <hr>
+ * Date created: Apr 13, 2016
+ * <hr>
+ * @author Matthew Moore
+ */
+public class Player
+{
+
+}
