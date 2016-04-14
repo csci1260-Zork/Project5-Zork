6ca8a8e6f4b4d6214b1bf4534d492e33ec77b39c
iveyas <iveyas@goldmail.etsu.edu>
2016-04-13 22:31:12 -0400

    Added files via upload

---
+++ Boglin.java
@@ -0,0 +1,38 @@
+/**
+ * ---------------------------------------------------------------------------
+ * File name: Boglin.java
+ * Project name: Project5-Zork
+ * ---------------------------------------------------------------------------
+ * Creator's name and email: Allison Ivey, iveyas@goldmail.etsu.edu
+ * Course:  CSCI 1260
+ * Creation Date: Apr 13, 2016
+ * ---------------------------------------------------------------------------
+ */
+
+package dungeonCrawl;
+
+
+/**
+ * Enter type purpose here
+ *
+ * <hr>
+ * Date created: Apr 13, 2016
+ * <hr>
+ * @author Allison Ivey
+ */
+public class Boglin extends Enemy
+{
+	
+	/**
+	 * Constructor        
+	 *
+	 * <hr>
+	 * Date created: Apr 13, 2016 
+	 *
+	 * 
+	 */
+	public Boglin ( )
+	{
+		super(20, .2, 20);
+	}
+}
---
+++ Ragdon.java
@@ -0,0 +1,39 @@
+/**
+ * ---------------------------------------------------------------------------
+ * File name: Ragdon.java
+ * Project name: Project5-Zork
+ * ---------------------------------------------------------------------------
+ * Creator's name and email: Allison Ivey, iveyas@goldmail.etsu.edu
+ * Course:  CSCI 1260
+ * Creation Date: Apr 13, 2016
+ * ---------------------------------------------------------------------------
+ */
+
+package dungeonCrawl;
+
+
+/**
+ * Enter type purpose here
+ *
+ * <hr>
+ * Date created: Apr 13, 2016
+ * <hr>
+ * @author Allison Ivey
+ */
+public class Ragdon extends Enemy 
+{
+
+	/**
+	 * Constructor        
+	 *
+	 * <hr>
+	 * Date created: Apr 13, 2016 
+	 *
+	 * 
+	 */
+	public Ragdon ( )
+	{
+		super(20, .15, 30);
+	}
+
+}
