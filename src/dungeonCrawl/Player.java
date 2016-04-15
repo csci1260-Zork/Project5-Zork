676f4d9ec003ceb457831f10adf91e18d1eb3058
Matthew <Matthew@MattMo>
2016-04-15 01:28:51 -0400

    trying to pull from the repository

--- src/dungeonCrawl/Player.java
+++ src/dungeonCrawl/Player.java
@@ -20,8 +20,75 @@ package dungeonCrawl;
  * <hr>
  * @author Matthew Moore
  */
-public class Player
+public class Player extends Participant
 {
+	private Weapon weapon;
+	private int potions;
+	private int attackPower;
+	
+	public Player()
+	{
+		
+	}
+	
+	public Player(Weapon weapon, int potions)
+	{
+		
+	}
+	
+	public void setWeapon(Weapon weapon){
+		
+	}
+	
+	public Weapon getWeapon()
+	{
+		return weapon;
+	}
+	
+	public void setPotions(int potions)
+	{
+		
+	}
+	
+	public int getPotions()
+	{
+		return potions;
+	}
+	
+	public void setAttackPower(int attackPower)
+	{
+		
+	}
+	
+	public int getAttackPower()
+	{
+		return attackPower;
+	}
+	
+	/**
+	 * Restores a portion of the Player's health.
+	 * Not sure what the value of health restored should be. Making it 10hp for now.     
+	 *
+	 * <hr>
+	 * Date created: Apr 14, 2016
+	 *
+	 * <hr>
+	 */
+	public void drinkPotion()
+	{
+		currentHealth += 10;
+	}
+	
+	public void addPotion()
+	{
+		potions++;
+	}
+	
+	
+	
+	
+	
+	
 	
 	
 
